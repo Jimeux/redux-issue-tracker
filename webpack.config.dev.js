6d0e33104318f4c28a133789317484e0a7425edd
James Kirk <jimeux@gmail.com>
2015-12-19 16:02:30 +0900

    Style and config tweaks

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,38 @@
 const path = require('path')
 const webpack = require('webpack')
 
+const node_modules_dir = path.join(__dirname, 'node_modules')
+const deps = [
+  //'react/dist/react.min.js',
+  //'react-dom/dist/react-dom.min.js',
+  //'react-addons-css-transition-group/index.js',
+  //'react-router/dist/react-router.min.js',
+  'moment/min/moment.min.js',
+  'babel-polyfill/dist/polyfill.min.js',
+  'jquery/dist/jquery.min.js'
+]
+
+const noParse = []
+const alias = {}
+
+deps.forEach((dep) => {
+  const depPath = path.resolve(node_modules_dir, dep)
+  alias[dep.split(path.sep)[0]] = depPath
+  noParse.push(depPath)
+})
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   context: __dirname,
 
-  entry:    [
+  entry: [
     'webpack/hot/dev-server',
     'webpack-hot-middleware/client',
     './src/entry'
   ],
 
   resolve: {
+    alias,
     modulesDirectories: ['src', 'node_modules']
   },
 
@@ -27,16 +48,18 @@ module.exports = {
   ],
 
   module: {
+    noParse,
     loaders: [
       {
         test: /\.js$/,
         loaders: ['babel'],
-        include: path.join(__dirname, 'src')
+        include: path.join(__dirname, 'src'),
+        exclude: path.join(__dirname, 'src', 'styles')
       },
       {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass'],
-        include: path.join(__dirname, 'src')
+        include: path.join(__dirname, 'src', 'styles')
       }
     ]
   }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,28 @@
 const path = require('path')
 const webpack = require('webpack')
 
+const node_modules_dir = path.join(__dirname, 'node_modules')
+
+const deps = [
+ // 'react/dist/react.min.js',
+  //'react-dom/dist/react-dom.min.js',
+  //'react-addons-css-transition-group/index.js',
+  //'react-router/dist/react-router.min.js',
+  'moment/min/moment.min.js',
+  'babel-polyfill/dist/polyfill.min.js',
+  'jquery/dist/jquery.min.js'
+]
+
+const noParse = []
+const alias = {}
+
+deps.forEach((dep) => {
+  const depPath = path.resolve(node_modules_dir, dep)
+  alias[dep.split(path.sep)[0]] = depPath
+  noParse.push(depPath)
+})
+
+
 module.exports = {
   devtool: 'source-map',
   entry: [
@@ -8,6 +30,7 @@ module.exports = {
   ],
 
   resolve: {
+    alias,
     modulesDirectories: ['src', 'node_modules']
   },
 
@@ -32,6 +55,7 @@ module.exports = {
   ],
 
   module: {
+    noParse,
     loaders: [
       {
         test: /\.jsx?$/,
